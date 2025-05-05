08d29df4e8eb0c8867ea5ec45a70a3abc51e04db
marvinernst1020 <me1020@gmx.de>
2025-05-06 00:44:41 +0200

    Added the global state ground truth.

--- hmm/creating_jags.R
+++ hmm/creating_jags.R
@@ -24,3 +24,39 @@ cat(
 }
 ', file = "poor_model.jags")
 
+
+##### Advanced Model:
+
+cat(
+  'model {
+  for (i in 1:K) {
+    for (t in 1:N) {
+      p_arm[i,t] <- z[t] * theta[i,2] + (1 - z[t]) * theta[i,1]
+    }
+  }
+
+  z[1] ~ dbern(0.5)
+  for (i in 1:K) {
+    y_obs[i,1] ~ dbern(p_arm[i,1])
+  }
+
+  for (t in 2:N) {
+    z[t] ~ dbern(z[t-1] * pi[2] + (1 - z[t-1]) * pi[1])
+    for (i in 1:K) {
+      y_obs[i,t] ~ dbern(p_arm[i,t])
+    }
+  }
+
+  pi[1] ~ dbeta(1,1)
+  pi[2] ~ dbeta(1,1)
+
+  for (i in 1:K) {
+    for (s in 1:2) {
+      theta[i,s] ~ dbeta(1,1)
+    }
+  }
+}
+', file = "advanced_model.jags")
+
+
+
